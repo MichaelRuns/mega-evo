70e3ea142e668dee0dffca0d6d9b923b848a4c59
Michael Vernau <102547230+MichaelRuns@users.noreply.github.com>
2023-10-16 09:25:36 -0700

    yeet yeet skrrt

--- frontend/src/components/ProjectDisplay.js
+++ frontend/src/components/ProjectDisplay.js
@@ -1,21 +1,20 @@
-import ProjectPresent from "./ProjectPresent";
 import Tile from "../headless/Tile";
 const projects = [
-    {
-      title: "Helius+",
-      text: "Point Cloud Perception",
-      description: "Served as the AI lead on a lidar-only perception & tracking system for smart infrastructure."
-    },
-    {
-      title: "Chidori",
-      text: "DCGAN Image Generation",
-      description: "Deployed a webapp equiped with a bespoke DCGAN to generate bird images."
-    },
-    {
-      title: "Koito calibration station",
-      text: "Robotics in Japan! Leadership",
-        description: "Led a foregin integration project in japan to automate and accelerate the calibration of lidars over 10x"
-    },
+  {
+    title: "Helius+",
+    text: "Point Cloud Perception",
+    img: "SLIDER-Appen_image_annotation_05.jpeg"
+  },
+  {
+    title: "Chidori",
+    text: "DCGAN Image Generation",
+    img: "mt_fuji.JPG"
+  },
+  {
+    title: "Koito calibration station",
+    text: "Robotics in Japan! Leadership",
+    img: "koito_mishima.JPG"
+  },
     // {
     //   title: "Pythia",
     //   text: "Applied ML to automate quality control on the production line",
@@ -26,7 +25,7 @@ const ProjectDisplay = () => {
  return (
     <div className="col-span-3 flex justify-between p-2 -translate-y-2">
          {projects.map((project, index) => (
-        <Tile key={index} title={project.title} text={project.text} desription={project.description}/>
+        <Tile key={index} title={project.title} text={project.text} desription={project.description} imagePath={project.img}/>
       ))}
     </div>
  );
--- frontend/src/headless/Tile.js
+++ frontend/src/headless/Tile.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { PlusIcon, MinusCircleIcon } from "@heroicons/react/solid";
 
-const Tile = ({ title, text, desription}) => {
+const Tile = ({ title, text, desription, imagePath}) => {
   const [isHovered, setIsHovered] = useState(false);
 
   return (
@@ -9,18 +9,26 @@ const Tile = ({ title, text, desription}) => {
       className={`relative rounded-lg transition-transform ${
         isHovered ? "scale-105" : "scale-100"
       } bg-zinc-100 p-4 ${
-        isHovered ? "h-full" : "h-1/3"
+        isHovered ? "h-full" : "h-1/2"
       } transition-all duration-300`}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
+      onClick={() => {
+        window.open("https://www.cepton.com/products-2023/helius", '_blank')
+      }}
     >
-        {/* Title */}
         <h2 className="text-xl font-semibold text-black mb-2">{text}</h2>
-        {/* Hero Icon (You can replace this with your custom HeroIcon) */}
-        {!isHovered? <PlusIcon className="w-8 h-8 text-black" /> : <MinusCircleIcon className="w-8 h-8 text-black" />}
-        {/* Text */}
         <p className="text-gray-600 mt-2">{title}</p>
-        {isHovered && <p> {desription}</p>}
+        <div className="h-full overflow-hidden justify-self-center"> {/* Adjust the height as needed */}
+                <img
+                    src={process.env.PUBLIC_URL + "/" + imagePath}
+                    alt="mike"
+                    className={`rounded object-cover border-2 border-black ${isHovered? "w-1/2" : "w-40 h-20"}`}
+                />
+            </div>
+        {isHovered && <div>
+           <p> {desription}</p>
+          </div>}
       </div>
   );
 };
