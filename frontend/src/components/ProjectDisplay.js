f731c68ba559ea2323f3fb0d5a441c4d1f657237
Michael Vernau <102547230+MichaelRuns@users.noreply.github.com>
2023-10-17 16:30:28 -0700

    remove warnings for ci

--- frontend/src/components/ProjectDisplay.js
+++ frontend/src/components/ProjectDisplay.js
@@ -2,33 +2,6 @@ import Tile from "../headless/Tile";
 import { DigizoidContext } from "./ContextProvider";
 import React from "react";
 import { useContext} from "react";
-const projects = [
-  {
-    title: "Helius+",
-    text: "Point Cloud Perception",
-    img: "SLIDER-Appen_image_annotation_05.jpeg",
-    description: "lorem ipsum",
-    link: "https://www.cepton.com/products-2023/helius"
-  },
-  {
-    title: "Chidori",
-    text: "DCGAN Image Generation",
-    img: "chidori.png",
-    description: "lorem ipsum",
-    link: "http://chidori.michaelvernau.com/"
-  },
-  {
-    title: "Koito calibration station",
-    text: "Automated Lidar Calibration",
-    img: "koito_squish.JPG",
-    description: "lorem ipsum lorem ipsum lorem ipsum"
-  },
-    // {
-    //   title: "Pythia",
-    //   text: "Applied ML to automate quality control on the production line",
-    // }
-    // Add more projects as needed
-  ];
 const ProjectDisplay = () => {
   const context = useContext(DigizoidContext);
  return (
