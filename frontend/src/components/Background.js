4d5392b127574ef90a047be0c609eca6609f09ab
Michael Vernau <102547230+MichaelRuns@users.noreply.github.com>
2023-10-16 18:11:49 -0700

    change bkg to use image

--- frontend/src/components/Background.js
+++ frontend/src/components/Background.js
@@ -1,9 +1,14 @@
-import React from 'react'
-const Background = ({children}) => {
-    return (
-        <div className="w-full h-full absolute top-0 left-0 bg-color bg-gradient-to-r from-blue-400 to-slate-600 grid grid-rows-[20%,80%] grid-cols-5">
-           {children}
-        </div>
-    );
-}
-export default Background
+import React from 'react';
+
+const Background = ({ children }) => {
+  return (
+      <div className="w-full h-full absolute top-0 left-0 grid grid-rows-[20%,80%] grid-cols-5"
+      style={{
+        backgroundImage: 'url(/const1.jpg)',
+      }}>
+        {children}
+      </div>
+  );
+};
+
+export default Background;
