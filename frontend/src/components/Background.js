bc831f9080c947776b48063ec10477e0942ada24
Michael Vernau <102547230+MichaelRuns@users.noreply.github.com>
2023-10-18 13:44:47 -0700

    allow background to overflow

--- frontend/src/components/Background.js
+++ frontend/src/components/Background.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Background = ({ children }) => {
   return (
-      <div className="w-full h-full absolute top-0 left-0 grid grid-rows-[20%,80%] grid-cols-5"
+      <div className="w-full h-full absolute top-0 left-0 grid grid-rows-[20%,80%] grid-cols-5 overflow-hidden"
       style={{
         backgroundImage: 'url(/const1.jpg)',
       }}>
